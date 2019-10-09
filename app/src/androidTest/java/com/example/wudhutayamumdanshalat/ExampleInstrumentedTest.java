629cc14eaf5e3235c7af1c5e8a14e540dcf01ed2
includedev8 <includedev8@gmail.com>
2019-10-09 19:45:52 +0700

    addition Activity

--- app/src/androidTest/java/com/example/wudhutayamumdanshalat/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wudhutayamumdanshalat/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.tatacarashalatdanwudhu", appContext.getPackageName());
+        assertEquals("com.example.wudhutayamumdanshalat", appContext.getPackageName());
     }
 }
